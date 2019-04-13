fd0de303eacd335cd2c30e901ce55f5e6f343f2d
zyklonex <zyklonex@gmail.com>
2019-04-13 15:44:44 -0600

    backend DB

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const low = require('lowdb')
+const FileAsync = require('lowdb/adapters/FileAsync')
+
+// Create server
+const app = express()
+app.use(bodyParser.json())
+
+// Create database instance and start server
+const adapter = new FileAsync('db.json')
+console.log('creating restful services for database')
+low(adapter)
+  .then(db => {
+    // Routes
+    // GET /todos/:id
+    app.get('/test/', (req, res) => {
+        console.log('inside get endpoint')
+      res.send({"test":"test"})
+    })
+    app.get('/todos/', (req, res) => {
+        console.log('entering endpoint get todos:')
+      const post = db.get('todos')
+
+        .value()
+        console.log('inside get endpoint:'+post)
+      res.send(post)
+
+    })
+    app.get('/todos/:id', (req, res) => {
+        console.log('entering endpoint get todos:'+req.params.id)
+      const post = db.get('todos')
+        .find({ id: Number(req.params.id) })
+        .value()
+        console.log('inside get endpoint:'+post)
+      res.send(post)
+    })
+app.post('/todos/', (req, res) => {
+    console.log('id passed in  ' + JSON.stringify(req.body))
+    db.get('todos')
+    .push(req.body)
+    .last()
+    .write()
+    .then(todos => res.send(todos))
+  })
+
+  app.delete('/todos/:id', (req, res) => {
+    console.log('deleting todos:'+Number(req.params.id))
+  const post = db.get('todos')
+    .remove({ id: Number(req.params.id) })
+    .write()
+    console.log('inside get endpoint:'+post)
+  res.send(post)
+})
+
+    // Set db default values
+    return db.defaults({ todos: [] }).write()
+  })
+  .then(() => {
+    app.listen(4000, () => console.log('listening on port 4000'))
+  })
